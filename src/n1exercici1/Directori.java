f953f93b7009f9b73b3a5db0b8aa28eb20c2de8c
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2022-04-06 11:27:05 +0200

    .
    .

--- src/n1exercici1/Directori.java
+++ src/n1exercici1/Directori.java
@@ -1,6 +1,16 @@
 package n1exercici1;
+
 import java.io.File;
-import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class Directori {
 	String[] pathnames;
@@ -8,14 +18,19 @@ public class Directori {
     // Creates a new File instance by converting the given pathname string
     // into an abstract pathname
     File f = new File("C:\\Adobe Photoshop CS6 Portable");{
-
     // Populates the array with names of files and directories
     pathnames = f.list();
-
     // For each pathname in the pathnames array
     for (String pathname : pathnames) {
+    	String caracter = ".";
         // Print the names of files and directories
-        System.out.println(pathname);
+        //System.out.println(pathname);
+        if (f.isDirectory()) {
+        	System.out.println(f.listFiles().toString());
+        	System.out.println(pathname+" Es un archivo");
+        	
+        }
+        else {System.out.println(pathname+" Es una carpeta");}
     }
 }
 }
